75a72ba73d5c50d8862b446f7174ccabbbec5917
Matej Mazur <matej.mazur@mail.com>
2018-06-23 16:40:02 +0200

    Update Bootswatch links

--- playground/src/app.js
+++ playground/src/app.js
@@ -45,88 +45,74 @@ const cmOptions = {
 const themes = {
   default: {
     stylesheet:
-      '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css'
   },
   cerulean: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/cerulean/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/cerulean/bootstrap.min.css'
   },
   cosmo: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/cosmo/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/cosmo/bootstrap.min.css'
   },
   cyborg: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/cyborg/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/cyborg/bootstrap.min.css',
     editor: 'blackboard'
   },
   darkly: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/darkly/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/darkly/bootstrap.min.css',
     editor: 'mbo'
   },
   flatly: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/flatly/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/flatly/bootstrap.min.css',
     editor: 'ttcn'
   },
   journal: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/journal/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/journal/bootstrap.min.css'
   },
   lumen: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/lumen/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/lumen/bootstrap.min.css'
   },
-  paper: {
+  litera: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/paper/bootstrap.min.css'
-  },
-  readable: {
-    stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/readable/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/litera/bootstrap.min.css'
   },
   sandstone: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/sandstone/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/sandstone/bootstrap.min.css',
     editor: 'solarized'
   },
   simplex: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/simplex/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/simplex/bootstrap.min.css',
     editor: 'ttcn'
   },
   slate: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/slate/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/slate/bootstrap.min.css',
     editor: 'monokai'
   },
   spacelab: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/spacelab/bootstrap.min.css'
-  },
-  'solarized-dark': {
-    stylesheet:
-      '//cdn.rawgit.com/aalpern/bootstrap-solarized/master/bootstrap-solarized-dark.css',
-    editor: 'dracula'
-  },
-  'solarized-light': {
-    stylesheet:
-      '//cdn.rawgit.com/aalpern/bootstrap-solarized/master/bootstrap-solarized-light.css',
-    editor: 'solarized'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/spacelab/bootstrap.min.css'
   },
   superhero: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/superhero/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/superhero/bootstrap.min.css',
     editor: 'dracula'
   },
   united: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/united/bootstrap.min.css'
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/united/bootstrap.min.css'
   },
   yeti: {
     stylesheet:
-      '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/yeti/bootstrap.min.css',
+      '//stackpath.bootstrapcdn.com/bootswatch/4.1.1/yeti/bootstrap.min.css',
     editor: 'eclipse'
   }
 };

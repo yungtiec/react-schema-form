cc013cbd05eac9e602d07d51103fa3c651b42acd
Matej Mazur <matej.mazur@mail.com>
2018-07-29 15:46:08 +0200

    test-utils.js: add testEach()

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 
+/**
+ * Tests each testCase.
+ *
+ * Every testCase must have a title prop (string)!
+ */
+export const testEach = testFn => {
+  return _test;
+
+  function _test(testCases) {
+    testCases.forEach(testCase => {
+      const _test = testCase.only
+        ? test.only
+        : testCase.skip
+          ? test.skip
+          : test;
+      _test(testCase.title, () => testFn(testCase));
+    });
+  }
+};
+
 export const initForm = (Form, initProps) => {
   return props => {
     const utils = render(<Form {...initProps} {...props} />);

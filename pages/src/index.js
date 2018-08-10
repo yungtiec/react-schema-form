dffec8d1b497f62eb65803b60d99ffe697100b91
Matej Mazur <matej.mazur@mail.com>
2018-08-10 12:34:59 +0200

    Add simple demo

--- pages/src/index.js
+++ pages/src/index.js
@@ -1,4 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<div>Hello World!</div>, document.getElementById('app'));
+import Form from '@react-schema-form/bootstrap';
+
+const schema = {
+  type: 'object',
+  properties: {
+    foo: {
+      type: 'string'
+    }
+  }
+};
+
+const uiSchema = {
+  foo: {
+    'ui:help': <p>This is a paragraph</p>
+  }
+};
+
+class App extends React.Component {
+  state = {
+    values: {}
+  };
+
+  handleSubmit = values => {
+    this.setState({ values });
+  };
+
+  render() {
+    return (
+      <Form schema={schema} uiSchema={uiSchema} onSubmit={this.handleSubmit} />
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));

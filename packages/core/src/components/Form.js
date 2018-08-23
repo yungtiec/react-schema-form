544f421cd729e0b9e4582ac66aabb267daac422b
Matej Mazur <matej.mazur@mail.com>
2018-08-23 13:31:46 +0200

    Update <Form>

--- packages/core/src/components/Form.js
+++ packages/core/src/components/Form.js
@@ -19,69 +19,39 @@ export default class Form extends Component {
     noHtml5Validate: false
   };
 
-  constructor(props) {
-    super(props);
-    this.state = this.getStateFromProps(props);
-  }
+  state = getStateFromProps(this.props);
 
-  componentWillReceiveProps(nextProps) {
-    this.setState(this.getStateFromProps(nextProps));
-  }
-
-  getStateFromProps(props) {
-    const state = this.state || {};
-    const schema = 'schema' in props ? props.schema : this.props.schema;
-    const uiSchema = 'uiSchema' in props ? props.uiSchema : this.props.uiSchema;
-    const edit = typeof props.formData !== 'undefined';
-    const liveValidate = props.liveValidate || this.props.liveValidate;
-    const mustValidate = edit && !props.noValidate && liveValidate;
-    const { definitions } = schema;
-    const formData = getDefaultFormState(schema, props.formData, definitions);
-    const retrievedSchema = retrieveSchema(schema, definitions, formData);
-
-    const { errors, errorSchema } = mustValidate
-      ? this.validate(formData, schema)
-      : {
-        errors: state.errors || [],
-        errorSchema: state.errorSchema || {}
-      };
-    const idSchema = toIdSchema(
-      retrievedSchema,
-      uiSchema['ui:rootFieldId'],
-      definitions,
-      formData,
-      props.idPrefix
-    );
-    return {
-      schema,
-      uiSchema,
-      idSchema,
-      formData,
-      edit,
-      errors,
-      errorSchema
-    };
+  static getDerivedStateFromProps(props, state) {
+    const keys = Object.keys(state.cachedProps);
+    const changed = keys.some(key => props[key] !== state.cachedProps[key]);
+    if (changed) {
+      return getStateFromProps(props, state);
+    }
+    return null;
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     return shouldRender(this, nextProps, nextState);
   }
 
-  validate(formData, schema = this.props.schema) {
-    const { validate, transformErrors } = this.props;
-    const { definitions } = this.getRegistry();
-    const resolvedSchema = retrieveSchema(schema, definitions, formData);
+  validate(formData) {
+    const { schema } = this.props;
+    const retrievedSchema = retrieveSchema(
+      schema,
+      schema.definitions,
+      formData
+    );
     return validateFormData(
       formData,
-      resolvedSchema,
-      validate,
-      transformErrors
+      retrievedSchema,
+      this.props.validate,
+      this.props.transformErrors
     );
   }
 
   renderErrors(ErrorListTemplate) {
-    const { errors, errorSchema, schema, uiSchema } = this.state;
-    const { showErrorList, formContext } = this.props;
+    const { errors, errorSchema } = this.state;
+    const { schema, uiSchema, showErrorList, formContext } = this.props;
 
     if (errors.length && showErrorList != false) {
       return (
@@ -171,6 +141,8 @@ export default class Form extends Component {
 
   render() {
     const {
+      schema,
+      uiSchema,
       children,
       safeRenderCompletion,
       id,
@@ -186,7 +158,7 @@ export default class Form extends Component {
       noHtml5Validate
     } = this.props;
 
-    const { schema, uiSchema, formData, errorSchema, idSchema } = this.state;
+    const { formData, errorSchema, idSchema } = this.state;
     const registry = this.getRegistry();
     const _SchemaField = registry.fields.SchemaField;
     const { ErrorListTemplate, SubmitTemplate } = registry.templates;
@@ -223,6 +195,55 @@ export default class Form extends Component {
       </form>
     );
   }
+}
+
+function getStateFromProps(props, state = {}) {
+  const { schema, uiSchema, liveValidate, noValidate } = props;
+  const edit = typeof props.formData !== 'undefined';
+  const mustValidate = edit && !noValidate && liveValidate;
+  const { definitions } = schema;
+  const formData = getDefaultFormState(schema, props.formData, definitions);
+  const retrievedSchema = retrieveSchema(schema, definitions, formData);
+
+  let errors, errorSchema;
+  if (mustValidate) {
+    const fromValidation = validateFormData(
+      formData,
+      retrievedSchema,
+      props.validate,
+      props.transformErrors
+    );
+    errors = fromValidation.errors;
+    errorSchema = fromValidation.errorSchema;
+  } else {
+    errors = state.errors || [];
+    errorSchema = state.errorSchema || {};
+  }
+
+  const idSchema = toIdSchema(
+    retrievedSchema,
+    uiSchema['ui:rootFieldId'],
+    definitions,
+    formData,
+    props.idPrefix
+  );
+  return {
+    cachedProps: {
+      schema,
+      uiSchema,
+      formData: props.formData,
+      liveValidate,
+      noValidate,
+      validate: props.validate,
+      transformErrors: props.transformErrors,
+      idPrefix: props.idPrefix
+    },
+    idSchema,
+    formData,
+    edit,
+    errors,
+    errorSchema
+  };
 }
 
 if (process.env.NODE_ENV !== 'production') {
